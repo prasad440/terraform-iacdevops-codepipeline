2ff75a60fe25d6440e19788208e468fb1a98b83c
prasad440 <prasadkovvuri440@gmail.com>
2023-09-16 20:51:44 +0530

    change of new pipeline

--- terraform-manifests/provider.tf
+++ terraform-manifests/provider.tf
@@ -12,9 +12,4 @@ provider "aws" {
 */
 # Terraform Block
 # Adding Backend as S3 for Remote State Storage
-backend "s3" {
-  bucket         = "terraform-on-aws-for-ec24"
-  key            = "iacdevops/dev/terraform.tfstate"
-  region         = "ap-northeast-1"
-  dynamodb_table = "iacdevops-dev-tfstate"
-}
+
